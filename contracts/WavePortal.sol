24e6114864900bc8185e223733e58553a606770d
rafi-vivify <mdrafi@vivifyhealthcare.com>
2022-04-13 12:43:49 +0600

    Basic code added

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart, And I ll be more smart I if I ll get continuos support of this community, Thanks BuildSpace for such awesome content"
+        );
+    }
+}
