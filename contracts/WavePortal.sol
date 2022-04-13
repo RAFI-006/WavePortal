55fe0d42d54e5aa84a2395dc8f01525f84a7aa89
rafi-vivify <mdrafi@vivifyhealthcare.com>
2022-04-13 17:31:08 +0600

    contract changes

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,11 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 import "hardhat/console.sol";
 
 contract WavePortal {
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart, And I ll be more smart I if I ll get continuos support of this community, Thanks BuildSpace for such awesome content"
+        console.log("Welcome to my Wave Portal"
         );
     }
+
+   uint256 totalWaves;
+   function wave(uint256 value) public {
+
+       totalWaves += value;
+       console.log("%s has waved %s times",msg.sender,value);
+        }
+
+
+        function getTotalWaves() public view returns (uint256) {
+           console.log("Total Waves = %d", totalWaves);
+           return totalWaves;
+        }
+
+      
+
+
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,25 @@
 const { expect } = require("chai");
 const hre = require("hardhat");
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-    console.log("My First Contract deployed to:", waveContract.address);
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave(5);
+  await waveTxn.wait();
+
+  waveTxn = await waveContract.connect(randomPerson).wave(10);
+  await waveTxn.wait();
+
+
+  waveCount = await waveContract.getTotalWaves();
   };
   
   const runMain = async () => {
